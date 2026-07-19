362953bde7fd27e87a0e27f7e203b2530debd132
jusabkhano7 <jusabkhan78615@gmail.com>
2026-07-19 20:27:38 +0530

    Test button

--- script.js
+++ script.js
@@ -1,13 +1,3 @@
 function generateContent() {
-
-let topic = document.getElementById("topic").value;
-
-if(topic==""){
-alert("Please enter a topic.");
-return;
-}
-
-document.getElementById("output").innerHTML =
-"You entered: " + topic;
-
+  alert("Button is working");
 }
